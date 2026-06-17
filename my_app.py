70fef1997a5baebb1f41a4ca0eff43df800c3434
CristoCatalan <cata.cristobal4@gmail.com>
2026-06-16 21:05:06 -0400

    Se realizaron cambios en el layout Ahora ya se inicia el proyecto

--- my_app.py
+++ my_app.py
@@ -1,17 +1,33 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QApplication
+from instr import * 
+from PyQt5.QtCore import Qt
 
 class MainWin(QWidget):
-    def __init__(self)
+    def __init__(self):
+        super().__init__()
         self.set_appear()
         self.initUI()
         self.connection()
         self.show()
 
     def set_appear(self):
-        pass
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
     def initUI(self):
-        pass
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text)
+        self.layout.addWidget(self.instruction)
+        self.layout.addWidget(self.button, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout)
     def connection(self):
         pass
 
-    
+
+app = QApplication([])
+nw = MainWin()
+app.exec_()
