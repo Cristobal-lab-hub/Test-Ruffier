0b649c1921e960307fb68317dc1ffe49f8a1033e
CristoCatalan <cata.cristobal4@gmail.com>
2026-06-16 20:42:55 -0400

    Constructures agregados

--- my_app.py
+++ my_app.py
@@ -1,4 +1,17 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
 
 class MainWin(QWidget):
-    pass
+    def __init__(self)
+        self.set_appear()
+        self.initUI()
+        self.connection()
+        self.show()
+
+    def set_appear(self):
+        pass
+    def initUI(self):
+        pass
+    def connection(self):
+        pass
+
+    
